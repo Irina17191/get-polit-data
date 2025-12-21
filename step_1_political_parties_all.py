b3ebe41f20e66c9abe77332d0ac21672be620b78
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2025-12-21 23:40:28 +0200

    Update step_1 and step_2 files

--- step_1_political_parties_all.py
+++ step_1_political_parties_all.py
@@ -10,167 +10,167 @@ headers = {
 }
 
 
-# функція яка різні чатини адреси складає в один рядок
+# функція різні чатини адреси складає в один рядок
 def to_one_line(parts: list) -> str:
     return ", ".join(filter(None, parts))
 
 def to_one_line_without_comma(parts: list) -> str:
     return " ".join(filter(None, parts))
 
+def run_step_1():
+    # список для збереження в Excel
+    results = []
+    page = 1
+    size = 10000
 
-# список для збереження в Excel
-results = []
-page = 1
-size = 10000
-
-while True:
-    payload = {
-        "filters": None,
-        "order": None,
-        "pager": {
-            "page": page,
-            "size": size
+    while True:
+        payload = {
+            "filters": None,
+            "order": None,
+            "pager": {
+                "page": page,
+                "size": size
+            }
         }
-    }
 
-    response = requests.post(url, headers=headers, json=payload)
-
-
-    if response.status_code != 200:
-        print(f"Помилка: {response.status_code}")
-        break
+        response = requests.post(url, headers=headers, json=payload, verify=False)
 
 
-    data = response.json()
-    polit_parties = data['results']['list']
-
-    if not polit_parties:     # якщо список порожній, зупиняємось
-        print("Не має даних у базі")
-        break
-
-    for party in polit_parties:
-
-        # head_info
-        head_info = ''
-        if party.get('head_info'):
-            head_info_parts = [
-                party['head_info'].get('head_last_name'),
-                party['head_info'].get('head_first_name'),
-                party['head_info'].get('head_middle_name')
-            ]
-            head_info = to_one_line_without_comma(head_info_parts)
+        if response.status_code != 200:
+            print(f"Помилка: {response.status_code}")
+            break
 
 
+        data = response.json()
+        polit_parties = data['results']['list']
 
-        # register_address
-        register_address = ''
-        if party.get('register_address'):
-            register_address_parts = [
-                party["register_address"].get('country'),
-                party['register_address'].get('post_index'),
-                party["register_address"].get('region'),
-                party["register_address"].get('district'),
-                party["register_address"].get('city'),
-                party["register_address"].get('street'),
-                party["register_address"].get('building'),
-                party["register_address"].get('building_part_num'),
-                party["register_address"].get('apartments'),
-                party["register_address"].get('common'),
-                party["register_address"].get('address_uk'),
-                party["register_address"].get('address_en')
-            ]
+        if not polit_parties:     # якщо список порожній, зупиняємось
+            print("Не має даних у базі")
+            break
 
-            register_address = to_one_line(register_address_parts)
+        for party in polit_parties:
+
+            # head_info
+            head_info = ''
+            if party.get('head_info'):
+                head_info_parts = [
+                    party['head_info'].get('head_last_name'),
+                    party['head_info'].get('head_first_name'),
+                    party['head_info'].get('head_middle_name')
+                ]
+                head_info = to_one_line_without_comma(head_info_parts)
 
 
 
-        # actual_address
-        actual_address = ''
-        if party.get("actual_address"):
+            # register_address
+            register_address = ''
+            if party.get('register_address'):
+                register_address_parts = [
+                    party["register_address"].get('country'),
+                    party['register_address'].get('post_index'),
+                    party["register_address"].get('region'),
+                    party["register_address"].get('district'),
+                    party["register_address"].get('city'),
+                    party["register_address"].get('street'),
+                    party["register_address"].get('building'),
+                    party["register_address"].get('building_part_num'),
+                    party["register_address"].get('apartments'),
+                    party["register_address"].get('common'),
+                    party["register_address"].get('address_uk'),
+                    party["register_address"].get('address_en')
+                ]
 
-            actual_address_parts = [
-                party["actual_address"].get("country"),
-                party["actual_address"].get("post_index"),
-                party["actual_address"].get("region"),
-                party["actual_address"].get("district"),
-                party["actual_address"].get("city"),
-                party["actual_address"].get("street"),
-                party["actual_address"].get("building"),
-                party["actual_address"].get("building_part_num"),
-                party["actual_address"].get("apartments"),
-                party["actual_address"].get("common"),
-                party["actual_address"].get("address_uk"),
-                party["actual_address"].get("address_en")
-            ]
-
-            actual_address = to_one_line(actual_address_parts)
-
-
-
-        # основні партії
-        results.append({
-            "id": party['id'],
-            "is_active": party['is_active'],
-            "party_type": "main",
-            "main_party_id": party['id'],
-            "code": party['code'],
-            "name": party['name'],
-            # print(party.get('web_site_url', '')),
-            "web_site_url": party.get('web_site_url') or '',
-            "actual_address_same_register": party['actual_address_same_register'],
-            "created_at": party['created_at'],
-            "updated_at": party['updated_at'],
-            "email": party['email'],
-            "phone": party['phone'],
-            "head_info": head_info,
-            "register_address": register_address,
-            "actual_address": actual_address,
-            "parent": party['parent']
-        })
+                register_address = to_one_line(register_address_parts)
 
 
 
-        if party.get("regional_offices"):
-            regional_offices = party.get("regional_offices")
-            for reg_office in regional_offices:
-                results.append({
-                    "id": reg_office['id'],
-                    "is_active": reg_office['is_active'],
-                    "party_type": 'regional',
-                    "main_party_id": party['id'],
-                    "code": reg_office['code'],
-                    "name": reg_office['name'],
-                    "web_site_url": None,
-                    "actual_address_same_register": None,
-                    "created_at": None,
-                    "updated_at": None,
-                    "email": None,
-                    "phone": None,
-                    "head_info": None,
-                    "register_address": None,
-                    "actual_address": None,
-                    "parent": None
-                })
+            # actual_address
+            actual_address = ''
+            if party.get("actual_address"):
+
+                actual_address_parts = [
+                    party["actual_address"].get("country"),
+                    party["actual_address"].get("post_index"),
+                    party["actual_address"].get("region"),
+                    party["actual_address"].get("district"),
+                    party["actual_address"].get("city"),
+                    party["actual_address"].get("street"),
+                    party["actual_address"].get("building"),
+                    party["actual_address"].get("building_part_num"),
+                    party["actual_address"].get("apartments"),
+                    party["actual_address"].get("common"),
+                    party["actual_address"].get("address_uk"),
+                    party["actual_address"].get("address_en")
+                ]
+
+                actual_address = to_one_line(actual_address_parts)
 
 
 
+            # основні партії
+            results.append({
+                "id": party['id'],
+                "is_active": party['is_active'],
+                "party_type": "main",
+                "main_party_id": party['id'],
+                "code": party['code'],
+                "name": party['name'],
+                # print(party.get('web_site_url', '')),
+                "web_site_url": party.get('web_site_url') or '',
+                "actual_address_same_register": party['actual_address_same_register'],
+                "created_at": party['created_at'],
+                "updated_at": party['updated_at'],
+                "email": party['email'],
+                "phone": party['phone'],
+                "head_info": head_info,
+                "register_address": register_address,
+                "actual_address": actual_address,
+                "parent": party['parent']
+            })
 
-    print(f"Сторінка {page} оброблена, отримано {len(polit_parties)} main партій")
-    page += 1
 
 
-# створюю DataFrame та зберігаємо в Excel
-df = pd.DataFrame(results)
-
-###
-print(f"Усього зібрано: {len(results)} записів")
-print(df['party_type'].value_counts())
-##
-
-df.to_excel("step_1_political_parties_all.xlsx", index=False)
-
-print("Дані збережено у step_1_political_parties_all.xlsx")
+            if party.get("regional_offices"):
+                regional_offices = party.get("regional_offices")
+                for reg_office in regional_offices:
+                    results.append({
+                        "id": reg_office['id'],
+                        "is_active": reg_office['is_active'],
+                        "party_type": 'regional',
+                        "main_party_id": party['id'],
+                        "code": reg_office['code'],
+                        "name": reg_office['name'],
+                        "web_site_url": None,
+                        "actual_address_same_register": None,
+                        "created_at": None,
+                        "updated_at": None,
+                        "email": None,
+                        "phone": None,
+                        "head_info": None,
+                        "register_address": None,
+                        "actual_address": None,
+                        "parent": None
+                    })
 
 
 
 
+        print(f"Сторінка {page} оброблена, отримано {len(polit_parties)} main партій")
+        page += 1
+
+
+    # створюю DataFrame та зберігаю в Excel
+    df = pd.DataFrame(results)
+
+    ###
+    print(f"Усього зібрано: {len(results)} записів")
+    print(df['party_type'].value_counts())
+    ##
+
+    df.to_excel("step_1_political_parties_all.xlsx", index=False)
+
+    print("Дані збережено у step_1_political_parties_all.xlsx")
+
+
+if __name__ == "__main__":
+    run_step_1()
--- step_2_party_reports_all.py
+++ step_2_party_reports_all.py
@@ -8,80 +8,86 @@ headers = {
 }
 
 
-# Читаю Excel-файл (файл має бути в тій самій папці, що й main.py)
-df = pd.read_excel("step_1_political_parties_all.xlsx")
+def run_step_2():
 
-# Фільтрую тільки ті партії, де тип == "main"
-# Беру колонку 'id'
-df_main = df[df["party_type"] == "main"]
-party_ids = df_main["id"].tolist()
+    # Читаю Excel-файл (файл має бути в тій самій папці, що й main.py)
+    df = pd.read_excel("step_1_political_parties_all.xlsx")
 
-results = []
+    # Фільтрую тільки ті партії, де тип == "main"
+    # Беру колонку 'id'
+    df_main = df[df["party_type"] == "main"]
+    party_ids = df_main["id"].tolist()
 
-# Проходимося по кожному id
-for party_id in party_ids:
-    url = f'https://politdata.nazk.gov.ua/api/v2/party/{party_id}/reports'
-    response = requests.post(url, headers=headers)
+    results = []
 
-    if response.status_code != 200:
-        print(f"Помилка для партії {party_id}: {response.status_code}")
-        continue
+    # Проходимося по кожному id
+    for party_id in party_ids:
+        url = f'https://politdata.nazk.gov.ua/api/v2/party/{party_id}/reports'
+        response = requests.post(url, headers=headers)
 
-    data = response.json()
-    reports_main = data['results']['list']
-    # reports_main = data.get('results', {}).get('list', [])
+        if response.status_code != 200:
+            print(f"Помилка для партії {party_id}: {response.status_code}")
+            continue
 
-
-    if not reports_main:
-        continue
+        data = response.json()
+        reports_main = data.get("results", {}).get("list", [])
+        # reports_main = data.get('results', {}).get('list', [])
 
 
-    for report in reports_main:
-        # Основний звіт
-        results.append({
-            "id": report['id'],
-            "schema_version": report['schema_version'],
-            "report_type": report['report_type'],
-            "year": report['year'],
-            "quarter": report['quarter'],
-            "party_id": report['party_id'],
-            "main_party_id": report['party_id'],
-            "party_code": None,
-            "party_name": None,
-            "is_party_office": report['is_party_office'],
-            "signed_date": report['signed_date'],
-            "created_date": report['created_date'],
-            "signatory_id": report['signatory_id'],
-            "status": None
-        })
+        if not reports_main:
+            continue
 
 
-        if report.get('regional_reports'):
-            # Регіональні звіти
-            reports_regional = report.get('regional_reports')
-            for report_r in reports_regional:
-                results.append({
-                    "id": report_r['id'],
-                    "schema_version": None,
-                    "report_type": "regional",
-                    "year": report_r['year'],
-                    "quarter": report_r['quarter'],
-                    "party_id": report_r['party_info']['id'],
-                    "main_party_id": report['party_id'],
-                    "party_code": report_r['party_info']['code'],
-                    "party_name": report_r['party_info']['name'],
-                    "is_party_office": None,
-                    "signed_date": report_r['signed_date'],
-                    "created_date": report_r['created_date'],
-                    "signatory_id": None,
-                    "status": report_r['status']
-                })
+        for report in reports_main:
+            # Основний звіт
+            results.append({
+                "id": report['id'],
+                "schema_version": report['schema_version'],
+                "report_type": report['report_type'],
+                "year": report['year'],
+                "quarter": report['quarter'],
+                "party_id": report['party_id'],
+                "main_party_id": report['party_id'],
+                "party_code": None,
+                "party_name": None,
+                "is_party_office": report['is_party_office'],
+                "signed_date": report['signed_date'],
+                "created_date": report['created_date'],
+                "signatory_id": report['signatory_id'],
+                "status": None
+            })
 
 
-# збереження в Excel
-df_reports = pd.DataFrame(results)
-df_reports.to_excel("step_2_party_reports_all.xlsx", index=False)
+            if report.get('regional_reports'):
+                # Регіональні звіти
+                reports_regional = report.get('regional_reports')
+                for report_r in reports_regional:
+                    results.append({
+                        "id": report_r['id'],
+                        "schema_version": None,
+                        "report_type": "regional",
+                        "year": report_r['year'],
+                        "quarter": report_r['quarter'],
+                        "party_id": report_r['party_info']['id'],
+                        "main_party_id": report['party_id'],
+                        "party_code": report_r['party_info']['code'],
+                        "party_name": report_r['party_info']['name'],
+                        "is_party_office": None,
+                        "signed_date": report_r['signed_date'],
+                        "created_date": report_r['created_date'],
+                        "signatory_id": None,
+                        "status": report_r['status']
+                    })
 
 
-print("Дані про звіти збережено у step_2_party_reports_all.xlsx")
+    # збереження в Excel
+    df_reports = pd.DataFrame(results)
+    df_reports.to_excel("step_2_party_reports_all.xlsx", index=False)
 
+
+    print("Дані про звіти збережено у step_2_party_reports_all.xlsx")
+
+
+
+if __name__ == "__main__":
+    run_step_2()
